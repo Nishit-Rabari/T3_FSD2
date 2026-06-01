dfbd7aa659ce71795f7a930aeb5d9d132653a715
Nishit_Rabari9999 <rabari.nishit08@gmail.com>
2026-06-01 10:03:41 +0530

    Add files via upload

---
+++ Comp.jsx
@@ -0,0 +1,12 @@
+import react ,{useContext} from 'react';
+import {Fname} from './Main1';
+function Comp(){
+    const value=useContext(Fname)
+    return(
+        <>
+        <span>{value}</span>
+        
+        </>
+    )
+}
+export default Comp;
